83b62b55bc73e7f660435f92cabcf68e65005d6d
parvezhossain671 <parvezhossain89@gmail.com>
2026-09-13 10:44:09 +0600

    Change MQTT_PASSWORD for testing
    Updated MQTT_PASSWORD for testing purposes.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const MQTT_PORT = 8884;
 // For testing only. Do NOT expose a production password
 // in browser JavaScript.
 const MQTT_USERNAME = "esp32";
-const MQTT_PASSWORD = "REPLACE_WITH_YOUR_HIVEMQ_PASSWORD";
+const MQTT_PASSWORD = "hackers game";
 
 // =====================================================
 // MQTT Topics
